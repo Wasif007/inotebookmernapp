56f3bbe1546b1bdd09bf9ba1b832caf5a9042f3a
Wasif007 <wasifateeq007@hotmail.com>
2023-07-24 22:00:44 +0500

    navbar setting done to start with

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Navbar from "./Components/Navbar";
 import './App.css';
+import About from "./Components/About";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <div className="App">
-     <h1>NOTEBOOK APPLICATION</h1>
-    </div>
+     <Router>
+          <Navbar /> 
+           <div className="App">
+          <h1>NOTEBOOK APPLICATION</h1>
+          </div>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home   exact/>}
+            />
+            <Route
+              path="/about"
+              element={ <About exact/>
+              }
+            />
+            </Routes>
+        </Router>
+ 
     </>
   );
 }
