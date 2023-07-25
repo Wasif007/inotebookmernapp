f8b1eced30b5560fa74159220c0c2f6eca85bf5c
Wasif007 <wasifateeq007@hotmail.com>
2023-07-25 22:19:59 +0500

    use of usestate and useeffect with contextapi

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useContext } from 'react'
 import createContextExp from '../context/routes/notecontext';
+import { useEffect } from 'react';
 const About = () => {
   const a= useContext(createContextExp);
+  useEffect(() => {
+    a.updateFunction();
+    // eslint-disable-next-line
+  }, []);
+ 
   console.log(a);
   return (
     <div>
-      This is about page {a.name}
+      This is about page {a.state.name} and his age is {a.state.age}
     </div>
   )
 }
--- src/context/routes/notestate.js
+++ src/context/routes/notestate.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import createContextExp from "./notecontext";
 
 const CreateContextExpFunction=(props)=>{
-    const state={
+    const s1={
         "name":"Kuch bhe rakh lo",
         "age":"Kuch bhe chale ge"
     }
+    const [state,statechange]=useState(s1);
+    const updateFunction=()=>{
+        setTimeout(() => {
+           statechange({
+            "name":"Jhinggaa ",
+        "age":"Lalallaal"
+        }) 
+        }, 1000);
+        
+    }
     return(
-        <createContextExp.Provider value={state}>
+        <createContextExp.Provider value={{state,updateFunction}}>
             {props.children}
         </createContextExp.Provider>
     )
